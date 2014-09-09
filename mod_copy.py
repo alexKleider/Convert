dc2e3edc6c90a166e886a88fdd646671c527d782
Alex Kleider <alex@kleider.ca>
2014-09-09 12:34:04 -0700

    Add ability to 'pick up where left off'
    Currently this code is running following a previously aborted run.
    Some issues regarding subtleties of symlinks are discussed at the very end
    of mod_copy.py.

--- mod_copy.py
+++ mod_copy.py
@@ -7,11 +7,15 @@
 """
 Provided as constants in the code are:
 1. name of an existing directory assumed to contain mp4 files beneath it.
-2. name of another as yet non-existent directory which will be created. 
+2. name of another possibly non-existent destination directory. 
 Everything under directory 1 will be moved to directory 2,
-in the process changing any mp4 files into ogv format.
-Changing the html code (in order that links are not broken, must be done
-separately.  (See sister program scan4html.py.)
+in the process changing any mp4 files into ogv format, and
+changing the text of html files in order that links are not broken.
+If the destinatioin directory already exists, it's content will
+be checked for each file to be copied over and if the file already exists,
+nothing will be done with regard to that file.  This means that if the
+script is ever interupted before completion, rerunning the script will not
+waste resources doing what's already been done before.
 """
 import os
 import sys
@@ -35,17 +39,6 @@ def log(entry):
     """Logs entry to LOGGING_FILE."""
     with (open(LOGGING_FILE, "a")) as f:
         f.write("{0}\n".format(entry))
-
-def setup_directories(old_dir, new_dir):
-    """Check that old_dir exists and create new_dir.
-
-    Program aborts if old_dir doesn't exist or if new_dir already exists.
-    """
-    if not os.path.isdir(old_dir):
-        sys.exit("Expected directory can't be found.")
-    if os.path.isdir(new_dir):
-        sys.exit("Destination directory already exists.")
-    os.mkdir(new_dir)
 
 def convert_file(source, destination):
     """Convert source file (assumed to be .mp4) to destination.ogv.
@@ -71,10 +64,14 @@ def convert_text(text, old, new):
         return text.replace(old[1:], new[1:])
 
 def modify_file(source, destination):
-    """Changes each instance of '.mp4' in source to '.ogv' in destination.
-
+    """Modifies the text in .html files.
+    
+    Changes each instance of '.mp4' in source to '.ogv' in destination.
     Returns True if modifications were necessary, False if not.
+    Does nothing (and returns None) if destination already exists.
     """
+    if os.path.isfile(destination):
+        return 
     with open(source, 'r', encoding="latin-1") as source_file:
         content = convert_text(source_file.read(), 
                                 OLD_SUFFIX,
@@ -93,8 +90,11 @@ def move_files(old_dir, new_dir):
     """Move all files from old_dir to new_dir converting mp4 into ogv.
 
     old_dir must already exit, new_dir need not.
-    Only mp4 files will be changed (to ogv;) all other files 
-    will be moved 'as is.'  If ogv files are found on the input side, this
+    mp4 files will be converted to ogv;
+    html files will be checked for refs and corrected as need be.
+    If destination files are already present, they will NOT be overwritten:
+    processing simply doesn't take place.
+    If ogv files are found on the input side, this
     will be logged and a notification sent to stdout.
     Depends on log function.
     """
@@ -104,7 +104,7 @@ def move_files(old_dir, new_dir):
     n_html_files_converted = 0
     log("""
 ####   Beginning new instance of mod_copy.py {}"""\
-                                    .format(datetime.datetime.now()[:24]))
+                                    .format(str(datetime.datetime.now())[:19]))
     for root, _, files in os.walk(old_dir):
         working_destination = \
         os.path.abspath(root).replace(os.path.abspath(old_dir),
@@ -118,25 +118,46 @@ def move_files(old_dir, new_dir):
             n_files_processed += 1
             destination = source.replace(os.path.abspath(old_dir),
                                         os.path.abspath(new_dir), 1)
+            if os.path.isfile(destination):
+                log("{}: {} $".format(str(datetime.datetime.now())[:19], 
+                                               destination))
+                continue
             if source.endswith(OLD_SUFFIX):
                 destination = destination.replace(OLD_SUFFIX, NEW_SUFFIX)
+                if os.path.isfile(destination):
+                    log("{}: {} $".format(str(datetime.datetime.now())[:19],
+                                                destination))
+                    continue
                 return_code = convert_file(source, destination)
-                log("{}: {} => ogv".format(datetime.datetime.now()[:24], source))
+                log("{}: {} => ogv".format(str(datetime.datetime.now())[:19], source))
                 n_video_file_conversion_attempts += 1
                 if return_code:
                     print("!!!!!!!!!Return_code is {}.".\
                                                     format(return_code))
                     n_failed_video_conversions += 1
+                    continue
             elif source.endswith(HTML_SUFFIX):
-                if modify_file(source, destination):
-                    log("{}: {} +".format(datetime.datetime.now()[:24], source))
+                htm_modified = modify_file(source, destination)
+                if html_modified == None:
+                    log("{}: {} $".format(str(datetime.datetime.now())[:19], source))
+                    continue
+                if html_modified:
+                    log("{}: {} +".format(str(datetime.datetime.now())[:19], source))
                     n_html_files_converted += 1
                 else:
-                    log("{}: {} -".format(datetime.datetime.now()[:24], source))
+                    log("{}: {} -".format(str(datetime.datetime.now())[:19], source))
             else:
+                if os.path.isfile(destination):  # I don't think this
+                                                # should ever happen!!
+                    log("{}: {} !!".format(str(datetime.datetime.now())[:19],
+                                                destination))
+                    continue
+
+                if os.path.islink(destination):
+                    continue
                 shutil.copyfile(source, destination, follow_symlinks=False) 
+                log("{}: {} -".format(str(datetime.datetime.now())[:19], source))
             shutil.copymode(source, destination, follow_symlinks=False) 
-            log("{}: {} -".format(datetime.datetime.now()[:24], source))
     log("""------------------
 Completed with {files_checked} files checked, {video} video conversions ({failures} failed,) and {html} html conversions."""\
     .format(files_checked = n_files_processed, 
@@ -147,8 +168,8 @@ Completed with {files_checked} files checked, {video} video conversions ({failur
 def main():
     """Get user confirmation before calling move_files."""
     response = input("""Assume existing directory '{}'
-    and that its contents are to be transfered to 
-    as yet not existing directory '{}'
+    and that its contents are to be transfered to a possibly not yet
+    existing directory '{}'
     converting any mp4 files to ogv format in the process
     as well as fixing the refs in the html files.
     OK to proceed? (Y/n) """.format(ROOT_DIR,
@@ -169,4 +190,17 @@ if __name__ == "__main__":
     print('"""')
     main()
 
+some_notes = """
+There are some problems that arise when dealing with links- in this case
+symbolic links.
+os.path.isfile(<symlink>) reports on the existence of the target, not the
+link itself.  In my use case, it was reporting false even thought the link
+was present and needed. The target was not present (in a part of the
+directory tree outside the directory under consideration.)
+os.path.islink(<symlink>) reports what is needed in this situation.
+shutil.copy(<symlink>) copies the target of the link, not the link itself.
+shutil.copy(<symlink>, follow_symlinks=False) changes the behavior to copy
+the link itself which is what was needed in my use case.  Existence of the
+target has no bearing.
+"""
 
